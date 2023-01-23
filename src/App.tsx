6cdff705bc216084498d55a6b8873212a80bcfd2
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-23 17:29:41 -0300

    feat: Add board component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ function App() {
   
   return (
     <Dashboard>
-      
+      <h3 className='text-white font-bold text-3xl'>Overview</h3>
+      <article className='mt-10'>
+        
+      </article>
+
+      <h3 className='text-white font-bold text-3xl'>Your desks</h3>
+      <article className='my-5 grid gap-5 md:grid-cols-2'>
+        <Board></Board>
+        <Board></Board>
+        <Board></Board>
+      </article>
+      <button className='px-5 py-2 rounded-3xl bg-teal-500'>Add a new desk</button>
     </Dashboard>
   )
 }
---
+++ src/components/Boards.tsx
@@ -0,0 +1,15 @@
+export function Board(){
+    return(
+        <div className="w-full p-5 bg-slate-900 rounded-3xl border-slate-800 border-2">
+            <b className="text-white">Desk name</b>
+            <span className="w-full flex gap-3 mt-3 pb-5 border-b-2 border-slate-700">
+                <div className="px-3 rounded-3xl bg-blue-100 text-sm">12 New</div>
+                <div className="px-3 rounded-3xl bg-red-100 text-sm">102 Learning</div>
+                <div className="px-3 rounded-3xl bg-green-100 text-sm">4 Review</div>
+            </span>
+            <div className="mt-5">
+                <p className="text-sm text-slate-300">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, expedita.</p>
+            </div>
+        </div>
+    )
+}
