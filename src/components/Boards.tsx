b5ca1abad1c825f422dba8a3b15eaf018ab3c6f9
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-23 19:47:12 -0300

    release: Add props

--- src/components/Boards.tsx
+++ src/components/Boards.tsx
@@ -1,14 +1,14 @@
-export function Board(){
+export function Board({title, description, newCards, learnCards, reviewCards}:{title:string, description:string, newCards: number, learnCards: number, reviewCards: number}){
     return(
         <div className="w-full p-5 bg-slate-900 rounded-3xl border-slate-800 border-2">
-            <b className="text-white text-lg">Desk name</b>
+            <b className="text-white text-lg">{title}</b>
             <span className="w-full grid grid-cols-2 sm:flex gap-3 mt-3 pb-5 border-b-2 border-slate-700">
-                <div className="px-3 rounded-3xl bg-blue-100 text-sm text-center">12 New</div>
-                <div className="px-3 rounded-3xl bg-red-100 text-sm text-center">102 Learn</div>
-                <div className="px-3 rounded-3xl bg-green-100 text-sm text-center">4 Review</div>
+                <div className="px-3 rounded-3xl bg-blue-100 text-sm text-center">{newCards} New</div>
+                <div className="px-3 rounded-3xl bg-red-100 text-sm text-center">{learnCards} Learn</div>
+                <div className="px-3 rounded-3xl bg-green-100 text-sm text-center">{reviewCards} Review</div>
             </span>
             <div className="mt-5">
-                <p className="text-sm text-slate-300">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, expedita.</p>
+                <p className="text-sm text-slate-300">{description}</p>
             </div>
         </div>
     )
