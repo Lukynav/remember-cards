fd324f8168653ac6fbe53cad509d9b69ad5f860b
Lucas Navarro <Lucasnav6@gmail.com>
2023-01-23 18:51:00 -0300

    feat: Add analytics and boards

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from './components/Analytics'
 import { Board } from './components/Boards'
 import { Dashboard } from './templates/dashboard'
 
@@ -6,17 +7,21 @@ function App() {
   return (
     <Dashboard>
       <h3 className='text-white font-bold text-3xl'>Overview</h3>
-      <article className='mt-10'>
-        
+      <article className='my-5 grid grid-cols-2 md:grid-cols-3 gap-5'>
+        <Analytics num={12} title={'Due'} description={'cards today'}/>
+        <Analytics num={78} title={'Studied'} description={'minutes today'}/>
+        <Analytics num={12} title={'New'} description={'cards'}/>
       </article>
 
-      <h3 className='text-white font-bold text-3xl'>Your desks</h3>
+      <h3 className='text-white font-bold text-3xl mt-10'>Your desks</h3>
       <article className='my-5 grid gap-5 md:grid-cols-2'>
         <Board></Board>
         <Board></Board>
         <Board></Board>
       </article>
-      <button className='px-5 py-2 rounded-3xl bg-teal-500'>Add a new desk</button>
+      <article className='flex justify-center mt-10'>
+        <button className='px-5 py-2 rounded-3xl bg-teal-400 font-bold'>Add a new desk</button>
+      </article>
     </Dashboard>
   )
 }
